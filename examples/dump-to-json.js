4ded30a1343208f886fbc89a4be07cb0db3ed819
Jaon <jamonsterr@gmail.com>
2019-01-07 00:15:33 -0300

    Add dump json files

--- examples/dump-to-json.js
+++ examples/dump-to-json.js
@@ -4,7 +4,7 @@ const kenney = require('../');
 
 Promise.all([1, 2].map(n => kenney.search({ page: n })))
   .then(data => {
-    fs.writeFile('dump.json', JSON.stringify(flatten(data), null, 2), () => {});
+    fs.writeFile('dump-search.json', JSON.stringify(flatten(data), null, 2), () => {});
   });
 
 kenney.getAsset({ assetId: 'castle-kit' })
